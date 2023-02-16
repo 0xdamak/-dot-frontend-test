f8e55f4882b210263b19f7e74bba2d6cb9518952
damak <akinlade.co@gmail.com>
2023-02-16 19:42:34 +0100

    ballot

--- src/Components/Ballot/Ballot.tsx
+++ src/Components/Ballot/Ballot.tsx
@@ -1,9 +1,51 @@
-const Ballot = () => {
-  return (
-    <div className='ballot'>
-      'Your Code Goes Here'
-    </div>
-  )
-}
+import { useState } from "react";
+import { INominee, ISelection } from "models";
+import Nominee from "Components/Nominee";
+import styles from "./Ballot.module.scss";
 
-export default Ballot;
+type Props = {
+  id: string;
+  index: number;
+  title: string;
+  nominees: INominee[];
+  getSelection: ({ category, nominee }: ISelection) => void;
+};
+
+export default function Ballot({
+  id,
+  index,
+  nominees,
+  title,
+  getSelection,
+}: Props) {
+  const [selectedNominee, setSelectedNominee] = useState<INominee | null>(null);
+
+  function handler(nominee: INominee) {
+    setSelectedNominee(nominee);
+    getSelection({
+      category: {
+        id,
+        title,
+      },
+      nominee,
+    });
+  }
+
+  return (
+    <section className={styles.container}>
+      <header>
+        <h1>{title}</h1>
+      </header>
+      <div>
+        {nominees.map((datum) => (
+          <Nominee
+            selectedNominee={selectedNominee}
+            selectNominee={(nominee) => handler(nominee)}
+            key={datum.id}
+            nominee={datum}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
